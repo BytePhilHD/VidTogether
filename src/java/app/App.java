1a118e68a4a483bc46c93e39a56687dd08475573
BytePhil <phitho2018@gmail.com>
2021-05-28 22:18:48 +0200

    Version 0.0.3

--- src/java/app/App.java
+++ src/java/app/App.java
@@ -3,6 +3,7 @@ package app;
 import io.javalin.Javalin;
 import io.javalin.core.util.FileUtil;
 import io.javalin.http.Context;
+import io.javalin.websocket.WsConnectContext;
 import org.eclipse.jetty.websocket.api.Session;
 import utils.Console;
 import utils.MessageType;
@@ -11,10 +12,12 @@ import utils.UpdateConnection;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
+import java.text.SimpleDateFormat;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class App {
 
@@ -22,14 +25,16 @@ public class App {
 
     private final UpdateConnection updateConnection;
     private final UpdateConnection.Downloader downloader;
+    private static Javalin app;
 
     public static App getInstance() {
         return instance;
     }
-    public String version = "0.0.2";
+    public String version = "0.0.3";
 
     public HashMap<String, Session> sessionHashMap = new HashMap<>();
-    public List<String> sessions = new ArrayList<>();
+    public HashMap<String, WsConnectContext> sessionctx = new HashMap<>();
+    public List<String> sessions1 = new ArrayList<>();
 
     public App() {
         instance = this;
@@ -42,15 +47,18 @@ public class App {
         Javalin app = Javalin.create(config -> {
             config.addStaticFiles("/public");
         }).start(70);
+        App.app = app;
 
         app.ws("/websockets", ws -> {
             ws.onConnect(ctx -> {
                 Console.printout("Client connected with Session-ID: " + ctx.getSessionId() + " IP: " + ctx.session.getRemoteAddress()
                     , MessageType.INFO); App.getInstance().sessionHashMap.put(ctx.getSessionId(), ctx.session);
-                        App.getInstance().sessions.add(ctx.getSessionId()); });
+                        App.getInstance().sessions1.add(ctx.getSessionId());
+                        ctx.send("Hi was geht"); sessionctx.put(ctx.getSessionId(), ctx); });
             ws.onClose(ctx -> { Console.printout("Client disconnected (Session-ID: " + ctx.getSessionId() + ")"
                     , MessageType.INFO); App.getInstance().sessionHashMap.remove(ctx.getSessionId());
-                        App.getInstance().sessions.remove(ctx.getSessionId()); });
+                        App.getInstance().sessions1.remove(ctx.getSessionId());
+                        sessionctx.remove(ctx.getSessionId());});
         });
 
         app.post("/upload-example", ctx -> {
@@ -90,10 +98,27 @@ public class App {
         }
     }
         System.out.println("");
-        input(app);
+        run();
+        input();
+    }
+    public void run() throws IOException {
+        // Eine Endlosschleife, die einmal pro Sekunde die Datei öffnet, und ausliest
+        while( true ){
+            for (int i = 0; i < App.getInstance().sessionHashMap.size(); i++) {
+                String sessionid = App.getInstance().sessions1.get(i);
+                WsConnectContext session = App.getInstance().sessionctx.get(sessionid);
+                DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss");
+                session.send("Aktuelle Zeit: " + ZonedDateTime.now(ZoneId.of("Europe/Berlin")).format(DateTimeFormatter.ofPattern("HH:mm:ss")));
+            }
+            try {
+                Thread.sleep( 500 );
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
-    private static void input(Javalin app) throws IOException {
+    private static void input() throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
         String input = reader.readLine();
@@ -108,27 +133,20 @@ public class App {
                 System.out.println("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=");
                 System.out.println(" Stop Program » exit");
                 System.out.println(" Show help » help");
-                App.input(app);
+                App.input();
             } case "stopweb": {
                 app.stop();
                 System.out.println("The Webserver will be stopped!");
             } case "list": {
                 Console.printout("All Connected Clients", MessageType.INFO);
                 for (int i = 0; i < App.getInstance().sessionHashMap.size(); i++) {
-                    String sessionid = App.getInstance().sessions.get(i);
+                    String sessionid = App.getInstance().sessions1.get(i);
                     Session session = App.getInstance().sessionHashMap.get(sessionid);
                     Console.printout(sessionid + " | IP: " + session.getRemoteAddress(), MessageType.INFO);
                 }
-                App.input(app);
+                App.input();
             }
-            App.input(app);
+            App.input();
         }
-    }
-
-    private static void renderHelloPage(Context ctx) {
-            HelloPage page = new HelloPage();
-            page.userName = "admin";
-            page.userpassword = "admin234";
-            ctx.render("startpage.html", Collections.singletonMap("page", page));
     }
 }
