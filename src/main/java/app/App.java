255120796bc1c1279efc5f3a94d6d656ef674c2c
BytePhil <phitho2018@gmail.com>
2021-05-28 10:04:30 +0200

    Addet input Method

--- src/main/java/app/App.java
+++ src/main/java/app/App.java
@@ -4,10 +4,13 @@ import io.javalin.Javalin;
 import io.javalin.core.util.FileUtil;
 import io.javalin.http.Context;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Collections;
 
 public class App {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Javalin app = Javalin.create().start(70);
 
         app.get("/", App::renderHelloPage);
@@ -18,6 +21,29 @@ public class App {
             });
             ctx.html("Deine Datei wurde erfolgreich hochgeladen!");
         });
+        input(app);
+    }
+
+    private static void input(Javalin app) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        String input = reader.readLine();
+
+        switch (input) {
+            case "exit":
+            case "stop": {
+                System.out.println("PROGRAM WILL EXIT");
+                System.exit(1);
+            }
+            case "help": {
+                System.out.println("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=");
+                System.out.println(" Stop Program » exit");
+                System.out.println(" Show help » help");
+            } case "stopweb": {
+                app.stop();
+                System.out.println("WebServer wurde gestoppt!");
+            }
+        }
     }
 
     private static void renderHelloPage(Context ctx) {
