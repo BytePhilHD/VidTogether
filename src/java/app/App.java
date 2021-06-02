623460254444517d42e951c79d6e5672894750ad
BytePhil <phitho2018@gmail.com>
2021-06-02 18:31:35 +0200

    Bugfixes

--- src/java/app/App.java
+++ src/java/app/App.java
@@ -10,11 +10,13 @@ import jline.console.ConsoleReader;
 import org.eclipse.jetty.websocket.api.Session;
 import utils.Console;
 import utils.MessageType;
+import utils.ServiceState;
 import utils.UpdateConnection;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.Reader;
 import java.text.SimpleDateFormat;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
@@ -33,11 +35,14 @@ public class App {
     public static App getInstance() {
         return instance;
     }
+
     public String version = "0.0.3";
 
     public HashMap<String, Session> sessionHashMap = new HashMap<>();
     public HashMap<String, WsConnectContext> sessionctx = new HashMap<>();
     public List<String> sessions1 = new ArrayList<>();
+
+    public ServiceState serviceState = ServiceState.STARTING;
 
     public App() {
         instance = this;
@@ -54,17 +59,24 @@ public class App {
 
         app.ws("/websockets", ws -> {
             ws.onConnect(ctx -> {
-                Console.printout("Client connected with Session-ID: " + ctx.getSessionId() + " IP: " + ctx.session.getRemoteAddress()
-                    , MessageType.INFO); App.getInstance().sessionHashMap.put(ctx.getSessionId(), ctx.session);
-                        App.getInstance().sessions1.add(ctx.getSessionId());
-                        ctx.send("Client connects..");
-                        sessionctx.put(ctx.getSessionId(), ctx);
+                if (serviceState == ServiceState.ONLINE) {
+                    Console.printout("Client connected with Session-ID: " + ctx.getSessionId() + " IP: " + ctx.session.getRemoteAddress()
+                            , MessageType.INFO);
+                    App.getInstance().sessionHashMap.put(ctx.getSessionId(), ctx.session);
+                    App.getInstance().sessions1.add(ctx.getSessionId());
+                    ctx.send("Client connects..");
+                    sessionctx.put(ctx.getSessionId(), ctx);
+                }
             });
-            ws.onClose(ctx -> { Console.printout("Client disconnected (Session-ID: " + ctx.getSessionId() + ")"
-                    , MessageType.INFO); App.getInstance().sessionHashMap.remove(ctx.getSessionId());
-                        App.getInstance().sessions1.remove(ctx.getSessionId());
-                        sessionctx.remove(ctx.getSessionId());});
-            ws.onError(ctx -> { Console.printout("Websocket Error", MessageType.ERROR);
+            ws.onClose(ctx -> {
+                Console.printout("Client disconnected (Session-ID: " + ctx.getSessionId() + ")"
+                        , MessageType.INFO);
+                App.getInstance().sessionHashMap.remove(ctx.getSessionId());
+                App.getInstance().sessions1.remove(ctx.getSessionId());
+                sessionctx.remove(ctx.getSessionId());
+            });
+            ws.onError(ctx -> {
+                Console.printout("Websocket Error", MessageType.ERROR);
                 ctx.send("ERROR");
             });
         });
@@ -82,32 +94,38 @@ public class App {
                 Console.printout("Database is currently in maintenance!", MessageType.ERROR);
                 return;
             }
+            Console.printout("Successfully connected to Update Server.", MessageType.INFO);
         } catch (Exception ex) {
             Console.printout("Server connection failed!", MessageType.ERROR);
         }
 
-        Console.printout("Successfully connected to Update Server.", MessageType.INFO);
-
-
-    {
-        if (!updateConnection.isLatest()) {
-            if (!updateConnection.latestIsBeta()) {
-                downloader.download();
+        {
+            if (!updateConnection.isLatest()) {
+                if (!updateConnection.latestIsBeta()) {
+                    downloader.download();
+                } else {
+                    Console.printout("You aren't up to date. Please download the latest version.", MessageType.WARNING);
+                }
             } else {
-                Console.printout("You aren't up to date. Please download the latest version.", MessageType.WARNING);
+                Console.printout("Your running on the latest Version! (" + version + ")", MessageType.INFO);
+                serviceState = ServiceState.ONLINE;
             }
-        } else {
-            Console.printout("Your running on the latest Version! (" + version + ")", MessageType.INFO);
         }
+        Console.empty();
+        if (!thread.isAlive()) {
+            thread.start();
+        }
+
+        Console.printout("All Services started! Waiting for Client connection on YourIP:" + app.port(), MessageType.INFO);
+        Console.empty();
+
+        input();
     }
-        System.out.println("");
-        //input();
-        thread.run();
-    }
-    Thread thread=new Thread() {
+
+    public Thread thread = new Thread() {
         @Override
         public void run() {
-            while (true) {
+            while (thread.isAlive()) {
                 for (int i = 0; i < App.getInstance().sessionHashMap.size(); i++) {
                     String sessionid = App.getInstance().sessions1.get(i);
                     WsConnectContext session = App.getInstance().sessionctx.get(sessionid);
@@ -115,7 +133,7 @@ public class App {
                     session.send("Aktuelle Zeit: " + ZonedDateTime.now(ZoneId.of("Europe/Berlin")).format(DateTimeFormatter.ofPattern("HH:mm:ss")));
                 }
                 try {
-                    Thread.sleep(500);
+                    thread.sleep(500);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
@@ -123,40 +141,50 @@ public class App {
         }
     };
 
-    private static void input() throws IOException {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        private static void input() throws IOException {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
-        String input = null;
-        try {
-             input = reader.readLine();
-        } catch (Exception e1) {
-
-        }
-
-        switch (input) {
-            case "exit":
-            case "stop": {
-                Console.printout("PROGRAM WILL EXIT", MessageType.WARNING);
-                System.exit(1);
-            }
-            case "help": {
-                System.out.println("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=");
-                System.out.println(" Stop Program » exit");
-                System.out.println(" Show help » help");
-                App.input();
-            } case "stopweb": {
-                app.stop();
-                System.out.println("The Webserver will be stopped!");
-            } case "list": {
-                Console.printout("All Connected Clients", MessageType.INFO);
-                for (int i = 0; i < App.getInstance().sessionHashMap.size(); i++) {
-                    String sessionid = App.getInstance().sessions1.get(i);
-                    Session session = App.getInstance().sessionHashMap.get(sessionid);
-                    Console.printout(sessionid + " | IP: " + session.getRemoteAddress(), MessageType.INFO);
+            String input = null;
+            try {
+                input = reader.readLine();
+                switch (Objects.requireNonNull(input)) {
+                    case "exit":
+                    case "stop": {
+                        shutdown();
+                    }
+                    case "help": {
+                        System.out.println("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=");
+                        System.out.println(" Stop Program » exit");
+                        System.out.println(" Show help » help");
+                        System.out.println(" Show all connected Clients » list");
+                        App.input();
+                    }
+                    case "stopweb": {
+                        app.stop();
+                        System.out.println("The Webserver will be stopped!");
+                    }
+                    case "list": {
+                        Console.printout("All Connected Clients", MessageType.INFO);
+                        for (int i = 0; i < App.getInstance().sessionHashMap.size(); i++) {
+                            String sessionid = App.getInstance().sessions1.get(i);
+                            Session session = App.getInstance().sessionHashMap.get(sessionid);
+                            Console.printout(sessionid + " | IP: " + session.getRemoteAddress(), MessageType.INFO);
+                        }
+                        App.input();
+                    }
+                    Console.printout("Command not found! Use \"help\" for Help!", MessageType.ERROR);
+                    App.input();
                 }
                 App.input();
+            } catch (Exception e1) {
+                Console.printout("Reader Error: " + e1.getMessage(), MessageType.ERROR);
             }
-            App.input();
+        }
+
+        public static void shutdown() {
+            App.getInstance().serviceState = ServiceState.STOPPING;
+            Console.printout("System is stopping!", MessageType.ERROR);
+            app.stop();
+            System.exit(0);
         }
     }
-}
--- src/java/utils/Console.java
+++ src/java/utils/Console.java
@@ -9,6 +9,9 @@ public class Console {
     public static void printout(String message, MessageType type) {
         System.out.println("[" + getTime() + "] " + type + " - " + message);
     }
+    public static void empty() {
+        System.out.println("");
+    }
     private static String getTime() {
         return new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date());
     }
